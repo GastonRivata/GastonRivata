927238599903a8f22bb5cbd70e5e06d0787d143f
GastonRivata <rivatagaston@gmail.com>
2022-07-13 20:01:11 -0300

    Portfolio

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Gastón Rivata';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'PORTFOLIO';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
